78e2e9da236a0b6da6df952fdfa57eb767ea82bd
uzuku333 <sumgu91@gmail.com>
2025-11-25 16:10:03 +0100

    Přidání funkce a cyklu pro výpočet průměru

--- znamky.js
+++ znamky.js
@@ -1,3 +1,16 @@
 const znamky = [1, 2, 1, 3, 2, 1, 4];
 
-console.log("Vstupní známky:", znamky);
+function analyzujProspech(pole) {
+    let soucet = 0;
+
+    // CYKLUS: Sečtení všech známek
+    for (let i = 0; i < pole.length; i++) {
+        soucet += pole[i];
+    }
+
+    // Výpočet průměru (SEKVENCE)
+    let prumer = soucet / pole.length;
+    console.log("Průměr je:", prumer);
+}
+
+analyzujProspech(znamky);
